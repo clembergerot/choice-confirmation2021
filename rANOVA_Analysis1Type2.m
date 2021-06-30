e19e472a522166c4e2aae18421c89bda0f39cafa
Clémence Bergerot <81424477+clembergerot@users.noreply.github.com>
2021-06-30 12:05:16 +0200

    Update rANOVA_Analysis1Type2.m
    Changing comments

--- rANOVA_Analysis1Type2.m
+++ rANOVA_Analysis1Type2.m
@@ -1,36 +1,25 @@
-file_name = 'SimusAnalysis1Type2sim.mat';
+%% rANOVA to assess effects of Congruency, Resources and Pattern on performance for Random agents, Design A
+
+file_name = 'SimusAnalysis1Type2sim.mat'; %performance summary of Random agents on Design A
 
 load(file_name);
 
-simus4aO = simus_summary.simus4aO;
-simus4aP = simus_summary.simus4aP;
+simus4aO = simus_summary.simus4aO; %4aO: 4-alpha with Optimistic bias in forced-choice trials
+simus4aP = simus_summary.simus4aP; %4aP: 4-alpha with Pessimistic bias in forced-choice trials
 
 Pattern = repelem(["OptOpt", "OptPes"], [1000 1000]);
 
 Pattern = transpose(categorical(Pattern));
 
-%24 measurements
+%2000 measurements
 measures = [simus4aO ; simus4aP];
 
-%rANOVA
+%rANOVA with mixed design
 datatable = table(string(Pattern), measures(:,3), measures(:,4), measures(:,5), measures(:,6));
 datatable.Properties.VariableNames = {'Pattern','Cong_LR','Cong_HR','Incong_LR','Incong_HR',};
-% When you have more than one repeated-measures factor, you must set up a table
-% to indicate the levels on each factor for each of your different variables.
-% Here is the command you need for this case:
 WithinStructure = table([1 1 2 2 ]',[1 2 1 2]','VariableNames',{'Congruency','Resources'});
-% The 4 different rows of the WithinStructure table correspond to the 4 different
-% columns, 'pre_A','pre_B','post_A','post_B', respectively, in your data table.
-% Each 'pre_A','pre_B','post_A','post_B' column is coded as 1/2 on the PrePost factor
-% and as 1/2 on the TreatAB factor.
-% (Added based on later comments:)
-% Indicate that the 1's and 2's of WithinStructure are category labels
-% rather than regression-type numerical covariates:
 WithinStructure.Congruency = categorical(WithinStructure.Congruency);
 WithinStructure.Resources = categorical(WithinStructure.Resources);
-% Now pass the WithinStructure table to fitrm so that it knows how the different
-% columns correspond to the different levels of the repeated-measures factors.
 rm = fitrm(datatable, 'Cong_LR,Cong_HR,Incong_LR,Incong_HR~Pattern','WithinDesign',WithinStructure);
-% Finally, you need to specify the repeated-measures factors again when you call ranova, like this:
 ranovatbl = ranova(rm,'WithinModel','Congruency*Resources');
 ranovatbl = ranovatbl(:,1:end);
